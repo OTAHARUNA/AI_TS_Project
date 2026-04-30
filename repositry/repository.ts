0ff113db9827c2258d43829546b7f57b260384f7
OTAHARUNA <radwimps.love867@gmail.com>
2026-04-30 15:53:51 +0900

    async化・update追加

--- repositry/repository.ts
+++ repositry/repository.ts
@@ -1,15 +1,26 @@
 class Repository<T extends {id:number}> {
     private items: T[] = [];
 
-    add(item: T) {
+    async add(item: T) {
         this.items.push(item);
     }
 
-    getAll() {
+    async getAll() {
         return this.items;
     }
 
-    findById(id: number) {
+    async findById(id: number) {
         return this.items.find(item => item.id === id);
+    }
+
+    async update(id: number, item: T) {
+        //最初findByIdで存在見てidと一致する配列を更新と思っていたが、後者が一般的でないかもでindex
+        const targetIndex = this.items.findIndex(item => item.id === id);
+
+        if (targetIndex === -1) {
+            return
+        }
+        // 更新
+        this.items[targetIndex] = item;
     }
 }
