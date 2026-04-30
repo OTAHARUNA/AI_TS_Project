4c2a00569c88c07b1b451ba0bef889354962b0b0
OTAHARUNA <radwimps.love867@gmail.com>
2026-04-30 16:01:56 +0900

    部分更新に変更

--- repositry/repository.ts
+++ repositry/repository.ts
@@ -13,14 +13,17 @@ class Repository<T extends {id:number}> {
         return this.items.find(item => item.id === id);
     }
 
-    async update(id: number, item: T) {
-        //最初findByIdで存在見てidと一致する配列を更新と思っていたが、後者が一般的でないかもでindex
+    // 部分更新
+    async update(id: number, item: Partial<T>) {
         const targetIndex = this.items.findIndex(item => item.id === id);
 
         if (targetIndex === -1) {
             return
         }
         // 更新
-        this.items[targetIndex] = item;
+        this.items[targetIndex] = {
+            ...this.items[targetIndex],
+            ...item
+        };
     }
 }
